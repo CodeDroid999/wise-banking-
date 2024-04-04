33aecf9b5afa4c671d68ae4ca9373ee7a87e8feb
CodeDroid99 <johnmwega2002@gmail.com>
2024-04-04 11:43:03 +0300

    dddddddddddddddd

---
+++ oauth-connect-popup/pages/env-setup.tsx
@@ -0,0 +1,3 @@
+// Renders a special page for setting up environment vars. You wouldn't need it on your app!
+export { EnvSetupPage as default } from '../../common/components/EnvSetupPage/EnvSetupPage';
+export { getServerSideProps } from '../../common/components/EnvSetupPage/getServerSideProps';
